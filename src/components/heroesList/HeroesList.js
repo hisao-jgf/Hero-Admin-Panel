7d837f2d4a87c3ca55db3b697fdbb65265f150d3
hisaojgf <hisaojgf@gmail.com>
2023-01-20 22:05:42 +0100

    Fixed re-rendering of HeroesListItem components

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,5 +1,5 @@
 import { useRequest } from '../../hooks/request.hook';
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
@@ -18,16 +18,16 @@ const HeroesList = () => {
             .catch(() => dispatch(heroesFetchingError()))
     }, []);
 
+    const onHeroDelete = useCallback((id) => {
+        request(`http://localhost:3001/heroes/${id}`, "DELETE")
+            .then(dispatch(heroDeleted(id)))
+            .catch(e => console.log(e));
+    }, [request])
+    
     if (heroesLoadingStatus === "loading") {
         return <Spinner />;
     } else if (heroesLoadingStatus === "error") {
         return <h5 className="text-center mt-5">Loading error...</h5>
-    }
-
-    const onHeroDelete = (id) => {
-        request(`http://localhost:3001/heroes/${id}`, "DELETE")
-            .then(dispatch(heroDeleted(id)))
-            .catch(e => console.log(e));
     }
 
     const renderHeroesList = (heroesList) => {
