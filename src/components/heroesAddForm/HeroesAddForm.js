18aebb0e3a6dead4f8a44aabf427a77defd1b658
hisaojgf <hisaojgf@gmail.com>
2023-01-21 15:21:07 +0100

    Added on-form-submit new hero creating and displaying, adding to local JSON database functionality

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,6 +1,40 @@
+import { useRequest } from '../../hooks/request.hook';
+import { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+
+import { heroCreated } from '../../actions';
+
 const HeroesAddForm = () => {
+    const {heroes} = useSelector(state => state);
+
+    const [heroName, setHeroName] = useState('');
+    const [heroDescription, setHeroDescription] = useState('');
+    const [heroElement, setHeroElement] = useState('');
+
+    const dispatch = useDispatch();
+    const {request} = useRequest();
+
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+
+        const newHero = {
+            id: heroes.length + 1,
+            name: heroName,
+            description: heroDescription,
+            element: heroElement
+        }
+
+        request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
+            .then(dispatch(heroCreated(newHero)))
+            .catch(e => console.log(e));
+
+        setHeroName('');
+        setHeroDescription('');
+        setHeroElement('');
+    }
+
     return (
-        <form className="border p-4 shadow-lg rounded">
+        <form className="border p-4 shadow-lg rounded" onSubmit={onSubmitForm}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label fs-4">New hero's name</label>
                 <input 
@@ -9,7 +43,9 @@ const HeroesAddForm = () => {
                     name="name" 
                     className="form-control" 
                     id="name" 
-                    placeholder="What is my name?"/>
+                    placeholder="What is my name?"
+                    value={heroName}
+                    onChange={(e) => setHeroName(e.target.value)} />
             </div>
 
             <div className="mb-3">
@@ -20,7 +56,9 @@ const HeroesAddForm = () => {
                     className="form-control" 
                     id="text" 
                     placeholder="What are my abilities?"
-                    style={{"height": '130px'}}/>
+                    style={{"height": '130px'}}
+                    value={heroDescription}
+                    onChange={(e) => setHeroDescription(e.target.value)} />
             </div>
 
             <div className="mb-3">
@@ -29,7 +67,9 @@ const HeroesAddForm = () => {
                     required
                     className="form-select" 
                     id="element" 
-                    name="element">
+                    name="element"
+                    value={heroElement}
+                    onChange={(e) => setHeroElement(e.target.value)} >
                     <option >I bend the element of...</option>
                     <option value="fire">Fire</option>
                     <option value="water">Water</option>
