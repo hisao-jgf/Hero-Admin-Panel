a88f6c263ec27961d6c98da8217e903a8ac22978
hisaojgf <hisaojgf@gmail.com>
2023-01-22 14:53:35 +0100

    Split different functionality into separate reducers and combined them

---
+++ src/reducers/heroes.js
@@ -0,0 +1,40 @@
+const initialState = {
+    heroes: [],
+    heroesLoadingStatus: 'idle',
+}
+
+const heroes = (state = initialState, action) => {
+    switch (action.type) {
+        case 'HEROES_FETCHING':
+            return {
+                ...state,
+                heroesLoadingStatus: 'loading'
+            }
+        case 'HEROES_FETCHED':
+            return {
+                ...state,
+                heroes: action.payload,
+                heroesLoadingStatus: 'idle'
+            }
+        case 'HEROES_FETCHING_ERROR':
+            return {
+                ...state,
+                heroesLoadingStatus: 'error'
+            }
+        case 'HERO_DELETED':
+            const newHeroesList = state.heroes.filter(hero => hero.id !== action.payload)
+            return {
+                ...state,
+                heroes: newHeroesList
+            }
+        case 'HERO_CREATED':
+            const newHeroesCreatedList = [...state.heroes, action.payload]
+            return {
+                ...state,
+                heroes: newHeroesCreatedList
+            }
+        default: return state
+    }
+}
+
+export default heroes;
