18745db96ebc81537348ba5a79e007e0afb91d2f
hisaojgf <hisaojgf@gmail.com>
2023-01-23 15:08:21 +0100

    Created combined fetching actions to optimize heroesList/Filters components initial render

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -3,7 +3,7 @@ import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
 
-import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
+import { fetchHeroes, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
@@ -26,10 +26,7 @@ const HeroesList = () => {
     const {request} = useRequest();
 
     useEffect(() => {
-        dispatch(heroesFetching());
-        request("http://localhost:3001/heroes")
-            .then(data => dispatch(heroesFetched(data)))
-            .catch(() => dispatch(heroesFetchingError()))
+        dispatch(fetchHeroes(request));
     }, []);
 
     const onHeroDelete = useCallback((id) => {
