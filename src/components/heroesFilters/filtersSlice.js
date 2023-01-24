5caac2025be91d6ff2205e7ef35ef0fe427c4615
hisaojgf <hisaojgf@gmail.com>
2023-01-24 14:22:02 +0100

    Distributed combined action creators to corresponding components slices with createAsyncThunk

--- src/components/heroesFilters/filtersSlice.js
+++ src/components/heroesFilters/filtersSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { useRequest } from "../../hooks/request.hook";
 
 const initialState = {
     filters: [],
@@ -6,19 +7,31 @@ const initialState = {
     activeFilter: 'all'
 }
 
+export const fetchFilters = createAsyncThunk(
+    'filters/fetchFilters',
+    () => {
+        const {request} = useRequest();
+        return request("http://localhost:3001/filters");
+    }
+)
+
 const filtersSlice = createSlice({
     name: 'filters',
     initialState,
     reducers: {
-        filtersFetching: state => {state.filtersLoadingStatus = 'loading'},
-        filtersFetched: (state, action) => {
-                state.filters = action.payload;
-                state.filtersLoadingStatus = 'idle';
-        },
-        filtersFetchingError: state => {state.filtersLoadingStatus = 'error'},
         activeFilterChanged: (state, action) => {
             state.activeFilter = action.payload;
         }
+    },
+    extraReducers: builder => {
+        builder
+            .addCase(fetchFilters.pending, state => {state.filtersLoadingStatus = 'loading'})
+            .addCase(fetchFilters.fulfilled, (state, action) => {
+                state.filters = action.payload;
+                state.filtersLoadingStatus = 'idle';
+            })
+            .addCase(fetchFilters.rejected, state => {state.filtersLoadingStatus = 'error'})
+            .addDefaultCase(() => {})
     }
 });
 
--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,26 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { useRequest } from '../../hooks/request.hook';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle',
 }
 
+export const fetchHeroes = createAsyncThunk(
+    'heroes/fetchHeroes',
+    () => {
+        const {request} = useRequest();
+        return request("http://localhost:3001/heroes");
+    }
+)
+
 const heroesSlice = createSlice({
     name: 'heroes',
     initialState,
     reducers: {
-        heroesFetching: state => {state.heroesLoadingStatus = 'loading'},
-        heroesFetched: (state, action) => {
-                state.heroes = action.payload;
-                state.heroesLoadingStatus = 'idle';
-        },
-        heroesFetchingError: state => {state.heroesLoadingStatus = 'error'},
         heroCreated: (state, action) => {
             state.heroes.push(action.payload);
         },
         heroDeleted: (state, action) => {
             state.heroes = state.heroes.filter(hero => hero.id !== action.payload);
         }
+    },
+    extraReducers: builder => {
+        builder
+            .addCase(fetchHeroes.pending, state => {state.heroesLoadingStatus = 'loading'})
+            .addCase(fetchHeroes.fulfilled, (state, action) => {
+                state.heroes = action.payload;
+                state.heroesLoadingStatus = 'idle';
+            })
+            .addCase(fetchHeroes.rejected, state => {state.heroesLoadingStatus = 'error'})
+            .addDefaultCase(() => {})
     }
 });
 
